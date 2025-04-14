03fcd78011020c1dcf7ff47e9a1f5b0ebe8d286b
Shahzod277 <shahzodozbekov7@gmail.com>
2025-04-14 14:38:06 +0500

    Initial commit

---
+++ src/main/java/uz/raqamli_talim/certedusystem/CertEduSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.raqamli_talim.certedusystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertEduSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertEduSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/raqamli_talim/certedusystem/CertEduSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.raqamli_talim.certedusystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertEduSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
