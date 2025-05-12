3f9a1cacd8755915091b5d0357278dfe12b26d63
Shahzod277 <shahzodozbekov7@gmail.com>
2025-05-12 17:03:07 +0500

    push32

--- src/main/java/uz/raqamli_talim/certedusystem/model/projection/CertificateProjection.java
+++ src/main/java/uz/raqamli_talim/certedusystem/model/projection/CertificateProjection.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public interface CertificateProjection {
     String getPinfl();
+    String getId();
     String getSerialNumber();
     String getFirtName();
     String getLastName();
--- src/main/java/uz/raqamli_talim/certedusystem/repository/CertificateRepository.java
+++ src/main/java/uz/raqamli_talim/certedusystem/repository/CertificateRepository.java
@@ -14,7 +14,7 @@ public interface CertificateRepository extends JpaRepository<Certificate, Intege
     @Query("SELECT c FROM Certificate c " +
             "WHERE c.pinfl =?1")
     List<Certificate> findAllByPinfl(String pinfl);
-    @Query(value = "SELECT c.pinfl as pinfl, c.serial_number as serialNumber, c.firt_name as firtName, c.last_name as lastName, c.father_name as fatherName, c.level as level, c.subject as subject, c.subject_id as subjectId, c.ball as ball, c.start_date as startDate, c.end_date as endDate, c.cert_number as certNumber, c.url as url FROM certificate c WHERE (:search IS NULL OR LOWER(CONCAT(c.pinfl, c.firt_name, c.last_name, c.father_name, c.serial_number, c.cert_number)) LIKE LOWER(CONCAT('%', :search, '%')))", nativeQuery = true)
+    @Query(value = "SELECT c.id ,c.pinfl as pinfl, c.serial_number as serialNumber, c.firt_name as firtName, c.last_name as lastName, c.father_name as fatherName, c.level as level, c.subject as subject, c.subject_id as subjectId, c.ball as ball, c.start_date as startDate, c.end_date as endDate, c.cert_number as certNumber, c.url as url FROM certificate c WHERE (:search IS NULL OR LOWER(CONCAT(c.pinfl, c.firt_name, c.last_name, c.father_name, c.serial_number, c.cert_number)) LIKE LOWER(CONCAT('%', :search, '%')))", nativeQuery = true)
 
     Page<CertificateProjection> getCertificates(@Param("search") String search, Pageable pageable);
 
